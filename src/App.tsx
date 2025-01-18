e8e91527bbb990bb8434f09f9570076d04d64fc3
Luan Chiodini <a.luanjc@gmail.com>
2025-01-18 17:51:49 -0300

    feat: Footer and visual part of the home page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Footer } from "./components/Footer/Footer";
 import { Header } from "./components/Header/Header";
 import { Menu } from "./components/Menu/Menu";
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Menu />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+export const Footer = () => {
+  return (
+    <footer>
+      <button>
+        Veja seu carrinho<i className="ri-shopping-cart-2-fill"></i>
+      </button>
+    </footer>
+  );
+};
